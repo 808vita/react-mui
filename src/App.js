9da2a7e81151f40ed59f6fb2cb5806566c464afd
Sudharsun <psu.fabulo@gmail.com>
2022-08-06 18:38:33 +0530

    mui - starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
-	return <div className="App">oof</div>;
+	return (
+		<div className="App">
+			<Header />
+			<MainLayout />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/layout/MainLayout.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+export default function SimpleContainer() {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Container maxWidth="sm">
+				<h1>oof</h1>
+			</Container>
+		</React.Fragment>
+	);
+}
