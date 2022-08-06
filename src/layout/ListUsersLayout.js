043da44d2fc4226e0d91583549f03a6c487acdff
Sudharsun <psu.fabulo@gmail.com>
2022-08-06 18:52:36 +0530

    starter files

---
+++ src/layout/ListUsersLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListUsersLayout = () => {
+	return <div>ListUsersLayout</div>;
+};
+
+export default ListUsersLayout;
---
+++ src/layout/RegisterUserLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterUserLayout = () => {
+	return <div>RegisterUserLayout</div>;
+};
+
+export default RegisterUserLayout;
