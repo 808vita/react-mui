d1e0778fa00d70ebd9147bde2cb8aa12581abff5
Sudharsun <psu.fabulo@gmail.com>
2022-08-07 13:20:34 +0530

    clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import MainLayout from "./layout/MainLayout";
+
 import { Routes, Route } from "react-router-dom";
 import ListUsersPage from "./pages/ListUsersPage";
 import RegisterUserPage from "./pages/RegisterUserPage";
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import { styled } from "@mui/material/styles";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
